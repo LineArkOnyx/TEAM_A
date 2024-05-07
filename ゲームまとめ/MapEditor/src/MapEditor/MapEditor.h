c5dd72910f58a2e1ce35b2f23c7934edecc1c629
LineArkOnyx <05vertulw@gmail.com>
2024-05-07 17:08:19 +0900

    mapeditor

--- ゲームまとめ/MapEditor/src/MapEditor/MapEditor.h
+++ ゲームまとめ/MapEditor/src/MapEditor/MapEditor.h
@@ -4,11 +4,11 @@
 #include "../MathPlus/MathPlus.h"
 #include "../Input/Input.h"
 
-// /* <<Switch で検索するとマップチップenumの追加に伴うswitch文へ飛べる */
+// /* <<focus で検索するとマップチップenumの追加に伴うswitch文へ飛べる */
 
 // 各マップのデータ数
-const int MAP_DATA_Y = 23;
-const int MAP_DATA_X = 40;
+const int MAP_DATA_Y = 32;
+const int MAP_DATA_X = 128;
 
 // マップファイルのパス
 const char MAP_DEFAULT[] = "Data/Map/map.csv";
@@ -20,17 +20,48 @@ const int MAP_SIZE = 32;
 class MapData
 {
 protected:
-	// マップチップ画像一覧
+	// マップチップ画像一覧 <<focus
 	enum MapChipType {
 		MAPCHIP_NONE = -1,
 
-		MAPCHIP_BLOCK,
-		MAPCHIP_KUSA,
+		MAPCHIP_BLOCK_0,		// ブロック無割れ
+		MAPCHIP_BLOCK_1,		// ブロック小割れ
+		MAPCHIP_BLOCK_2,		// ブロック中割れ
+		MAPCHIP_BLOCK_3,		// ブロック大割れ
+		MAPCHIP_SOIL,			// 土
+		MAPCHIP_SAND,			// 砂
+		MAPCHIP_WOOD_FLORE_0,	// オークのフローリング
+		MAPCHIP_WOOD_FLORE_1,	// シラカバのフローリング
+		MAPCHIP_WOOD_FLORE_2,	// マツのフローリング
+		MAPCHIP_ICE,			// 氷
+		MAPCHIP_SNOW,			// 雪
+		MAPCHIP_GLASS,			// 草
+		MAPCHIP_TREE_0,			// 木１
+		MAPCHIP_TREE_1,			// 木２
+		MAPCHIP_TILE,			// タイル
+		MAPCHIP_RAVA,			// マグマ
+		MAPCHIP_STONE_0,		// 石レンガ薄
+		MAPCHIP_STONE_1,		// 石レンガ濃
+		MAPCHIP_STONE_BOX,		// 石のブロック
+		MAPCHIP_STEAL,			// スチール
+		MAPCHIP_NET,			// 金網
+		MAPCHIP_017,
+		MAPCHIP_018,
+		MAPCHIP_019,
+		MAPCHIP_01A,
+		MAPCHIP_01B,
+		MAPCHIP_01C,
+		MAPCHIP_01D,
+		MAPCHIP_01E,
+		MAPCHIP_01F,
+
 
 		MAPCHIP_MAX
 	};
 	MapChipType CurrentMCT;
 
+	int ScreenPosX;	//スクリーンの描画地点をセットする
+	int ScreenPosY;
 	// 画像ハンドル
 	int imgHundle[MAPCHIP_MAX];
 
@@ -60,12 +91,12 @@ public:
 class MapEditor : public MapData
 {
 private:
-	enum ControleIndex {
+	enum ControleIndex {// <<focus
 		CONTROLE_NONE = -1,
 
-		CONTROLE_SET_MCT,
-		CONTROLE_SELECT_MCT,
-		CONTROLE_SAVE_MAP,
+		CONTROLE_SET_MCT,		//マップチップを選択する
+		CONTROLE_SELECT_MCT,	//設置するチップを選択する
+		CONTROLE_SAVE_MAP,		//マップのデータを保存する
 
 		CONTROLE_MAX,
 	};	
